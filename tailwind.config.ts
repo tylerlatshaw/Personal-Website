683092152a9a46c997433ccea2405bc3bd1edc4f
Tyler Latshaw <tylerlatshaw@gmail.com>
2024-08-25 18:53:25 -0400

    Initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
@@ -13,6 +14,9 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+    },
+    animation: {
+      "spin-slow": "spin 1.5s linear infinite",
     },
   },
   plugins: [],
