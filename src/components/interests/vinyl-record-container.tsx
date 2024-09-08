572db29613cc0271dc079059c0a8550993cf95bc
Tyler Latshaw <tylerlatshaw@gmail.com>
2024-09-08 14:49:22 -0400

    Fixed map error

--- src/components/interests/vinyl-record-container.tsx
+++ src/components/interests/vinyl-record-container.tsx
@@ -13,23 +13,21 @@ export default function VinylRecordContainer() {
     async function generateCards() {
         const data: RecordResultType[] = await getData();
 
-        return <>
-            {
-                data.map((record, index) => {
-                    return <div key={index}>
-                        <div className="flex items-stretch bg-gray-900 shadow-lg drop-shadow-lg rounded p-3 flex flex-1 flex-col justify-between w-100">
-                            <img src={record.properties.ImageUrl.files[0].file.url} alt={record.properties.Name.rich_text[0].plain_text} className="aspect-square w-full rounded" />
+        return data && data.map((record) => {
+            return <>
+                <div key={record.id}>
+                    <div className="flex items-stretch bg-gray-900 shadow-lg drop-shadow-lg rounded p-3 flex flex-1 flex-col justify-between w-100">
+                        <img src={record.properties.ImageUrl.files[0].file.url} alt={record.properties.Name.rich_text[0].plain_text} className="aspect-square w-full rounded" />
+                    </div>
+                    <div className="px-1 pt-2 pb-4 divide-y-2 divide-green-900 mx-auto text-center">
+                        <div className="space-y-1">
+                            <h3 className="leading-snug font-bold text-base">{record.properties.Name.rich_text[0].plain_text}</h3>
+                            <div className="mx-auto text-sm italic text-gray-200 leading-tight">{record.properties.Artist.select.name}</div>
                         </div>
-                        <div className="px-1 pt-2 pb-4 divide-y-2 divide-green-900 mx-auto text-center">
-                            <div className="space-y-1">
-                                <h3 className="leading-snug font-bold text-base">{record.properties.Name.rich_text[0].plain_text}</h3>
-                                <div className="mx-auto text-sm italic text-gray-200 leading-tight">{record.properties.Artist.select.name}</div>
-                            </div>
-                        </div>
-                    </div>;
-                })
-            }
-        </>;
+                    </div>
+                </div>
+            </>;
+        });
     }
 
 
